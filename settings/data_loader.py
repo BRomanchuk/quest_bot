098b28fc24bf9264a8f4864069db32ff054735ff
BRomanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-09-07 23:23:19 +0300

    init data loader and quest functions

---
+++ settings/data_loader.py
@@ -0,0 +1,7 @@
+
+def get_quest_df():
+    pass
+
+
+def get_users_df():
+    pass
--- structure/quest.py
+++ structure/quest.py
@@ -1,4 +1,15 @@
+from settings.data_loader import get_quest_df
+
 
 class Quest:
     def __init__(self):
+        self.df = get_quest_df()
+
+    def get_task(self, task_num):
+        pass
+
+    def get_hint(self, task_num):
+        pass
+
+    def is_correct(self, task_num, answer):
         pass
